0987e84fc70ce4fb4b4336ad6fb47eef4e02c479
Pedro Lucas <plpvalgas@gmail.com>
2025-10-16 10:05:08 -0300

    Estrutura inicial do projeto criada

---
+++ src/main/java/com/pedrolucas/Agaply/AgaplyApplication.java
@@ -0,0 +1,13 @@
+package com.pedrolucas.Agaply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgaplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgaplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrolucas/Agaply/AgaplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrolucas.Agaply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgaplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
